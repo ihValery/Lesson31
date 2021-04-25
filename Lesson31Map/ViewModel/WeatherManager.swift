7609cf40d08725ccc6662580600ec488636bfa4a
Valery <ihValery@gmail.com>
2021-04-25 21:31:35 +0300

    Up

--- Lesson31Map/ViewModel/WeatherManager.swift
+++ Lesson31Map/ViewModel/WeatherManager.swift
@@ -18,12 +18,5 @@ class WeatherManager {
         self.networkDataFetcher = networkDataFetcher
     }
     
-    ///Create a link ready for sending (with: coordinate, and: key)
-    func createLinkGetWeatherData(coordinates: CLLocationCoordinate2D, completion: @escaping (WeatherData?) -> Void) {
-        let latitude = coordinates.latitude
-        let longitude = coordinates.longitude
-        let urlString = "\(Const.API.url)&appid=\(Const.API.key)&lat=\(latitude)&lon=\(longitude)"
-        
-        networkDataFetcher.fetchJSON(urlString: urlString, completionResponse: completion)
-    }
+
 }
